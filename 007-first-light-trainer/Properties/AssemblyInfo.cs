e6b763a718333539e9a4fac13e1f304724c229af
GateDeskIllustrate <dodie5069@gmail.com>
2026-07-07 06:09:27 +0700

    feat: add 007-first-light-trainer/Properties/AssemblyInfo.cs

---
+++ 007-first-light-trainer/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("007 First Light Trainer")]
+[assembly: AssemblyDescription("A simple trainer for 007 First Light")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("007 First Light Trainer")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d3b9c8a1-2f4e-4c6d-8b1a-0e9f8d7c6b5a")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
